8815fafc46d2ef0980b5f168394980536f95418f
Andrew Klar <andrewklar@hotmail.com>
2016-06-02 15:24:32 -0400

    completed cats.rb

--- cat.rb
+++ cat.rb
@@ -2,17 +2,30 @@ class Cat
 
 attr_accessor :name, :preferred_food, :meal_time
 
+
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
   end
 
+  def eats_at
+    @meal_time%12
+  end
+
+  def meow
+    if @meal_time > 12
+      puts "My name is #{name} and i eat #{preferred_food} at #{meal_time%12}"
+    else
+      puts "My name is #{name} and i eat #{preferred_food} at #{meal_time%12}"
+    end
+  end
 
 end
 
 
-pablo = Cat.new("Pablo", "Enchiladas", 11)
-steve = Cat.new("Steve", "Fish", 23)
+pablo = Cat.new("Pablo", "enchiladas", 11)
+steve = Cat.new("Steve", "fish", 23)
 
-puts pablo
+puts pablo.meow
+puts steve.meow
