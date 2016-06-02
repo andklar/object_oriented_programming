ba6f0ef8e48b3841b3b2543ff1c2b38c589268ef
Andrew Klar <andrewklar@hotmail.com>
2016-06-02 16:51:29 -0400

    finished all methods, need to adjust for odd/even

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -17,10 +17,22 @@ attr_reader :earnings
   end
 
   def deliver(start_address, end_address)
-    (end_address - start_address) / 2
+    delivered = (end_address - start_address) / 2
     @exp += ((end_address - start_address) / 2)
+      if delivered < @quota
+          @earnings += (0.25 * delivered) - 2
+      elsif delivered >= @quota
+          @earnings += (@quota * 0.25) + ((delivered - @quota) * 0.50)
+      end
+    puts @exp
+  end
+
+  def report
+    "I\'m #{name}, I\'ve delivered #{exp} papers and I\'ve earned $#{earnings} so far!"
   end
 
 end
+
+
 
 tommy = Paperboy.new("Tommy", 50, 0, "ODD", 0)
