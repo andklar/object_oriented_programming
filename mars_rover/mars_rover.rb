842f8e280c4065f5176a9206e4132d28cd982537
Andrew Klar <andrewklar@hotmail.com>
2016-06-03 10:58:39 -0400

    built read_instruct method and added input questions, need to connect input to instance methods and object states

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -10,68 +10,82 @@ class Rover
 
 
   def move
-    if @direction == "N"
-      @y += 1
-    elsif @direction == "E"
-      @x += 1
-    elsif @direction == "S"
-      @y -= 1
-    elsif @direction == "W"
-      @x -= 1
-    end
+      if @direction == "N"
+        @y += 1
+      elsif @direction == "E"
+        @x += 1
+      elsif @direction == "S"
+        @y -= 1
+      elsif @direction == "W"
+        @x -= 1
+      end
   end
 
 
   def turn_left
-    if @direction == "N"
-      @direction = "W"
-    elsif @direction == "E"
-      @direction = "N"
-    elsif @direction == "S"
-      @direction = "E"
-    elsif @direction == "W"
-      @direction = "S"
-    end
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "E"
+        @direction = "N"
+      elsif @direction == "S"
+        @direction = "E"
+      elsif @direction == "W"
+        @direction = "S"
+      end
   end
 
   def turn_right
-    if @direction == "N"
-      @direction = "E"
-    elsif @direction == "E"
-      @direction = "S"
-    elsif @direction == "S"
-      @direction = "W"
-    elsif @direction == "W"
-      @direction = "N"
-    end
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "N"
+      end
   end
 
+  puts "What are the dimensions of the plateau?"
+  size = gets.chomp
+
+  puts "What is my x coordinate?"
+  @x = gets.chomp
+
+  puts "What is my y coordinate?"
+  @y = gets.chomp
+
+  puts "What direction am i facing?"
+  @direction = gets.chomp.upcase
+
   def read_instruct
-      command = "MMMMMRMRMMMMM"
-      command.split('')
-    end
+      puts "What are my instructions? (L / R / M)"
+      instructions = gets.chomp.upcase
+
+      commands = instructions.split(" ")
+      commands.each do |command|
+        if command == "L"
+          turn_left
+        elsif command == "R"
+          turn_right
+        elsif command == "M"
+          move
+        end
+      end
+  end
 
 end
 
 
 
-# puts "What is the x dimension of the plateau?"
-# gets.chomp
-#
-# puts "What is the y dimension of the plateau?"
-# gets.chomp
-#
-# puts "What is my x coordinate?"
-# gets.chomp
-#
-# puts "What is my y coordinate?"
-# gets.chomp
-#
-# puts "What direction am i facing?"
-# gets.chomp.upcase
-#
-# puts "Should I turn left (L), right (R)"
-# gets.chomp.upcase
+
+
+
+alpha = Rover.new(0, 0, "N")
+bravo = Rover.new(2, 2, "N")
+charlie = Rover.new(4, 4, "N")
+
+
 #
 #
 #
